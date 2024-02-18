e990ab5d11bc904796cd5298bd77ea994e214ac3
Oleksandr Lutsenko <sashalutsenko1991@gmail.com>
2024-02-18 10:40:39 +0100

    q

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,45 +1,46 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import './App.css';
-import {Destriction} from '../Description/Description';
-import {Options} from '../Options/Options';
-import {Feedback} from '../Feedback/Feedback';
-import {Notification} from '../Notification/Notification';
+import { Description } from '../Description/Description';
+import { Options } from '../Options/Options';
+import { Feedback } from '../Feedback/Feedback';
+import { Notification } from '../Notification/Notification';
 
 export const App = () => {
-    const [feedback, setFeedback] = useState(
-        () => JSON.parse(localStorage.getItem('saved-feedback')) ?? {good: 0, neutral: 0, bad: 0}
-    );
-    const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const [feedback, setFeedback] = useState(
+    () => JSON.parse(localStorage.getItem('saved-feedback')) ?? { good: 0, neutral: 0, bad: 0 }
+  );
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
-    useEffect(() => {
-        localStorage.setItem('saved-feedback', JSON.stringify(feedback));
-    }, [feedback]);
+  useEffect(() => {
+    localStorage.setItem('saved-feedback', JSON.stringify(feedback));
+  }, [feedback]);
 
-    const addFeedback = feedbackType => {
-        setFeedback(prevFeedback => ({
-            ...prevFeedback,
-            [feedbackType]: prevFeedback[feedbackType] + 1,
-        }));
-    };
+  const addFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
 
-    const resetFeedback = () => {
-        setFeedback({good: 0, neutral: 0, bad: 0});
-    };
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
 
-    return (
-        <div>
-            <Destriction />
-            <Options 
-            feedback = {feedback}
-            addFeedback = {addFeedback}
-            resetFeedback = {resetFeedback}
-            totalFeedback = {totalFeedback}
-            />
-            {totalFeedback ? (
-                <Feedback feedback = {feedback} totalFeedback = {totalFeedback}/>
-            ) : (
-                <Notification>No feedback yet</Notification>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      <Description />
+      <Options
+        feedback={feedback}
+        addFeedback={addFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+
+      {totalFeedback ? (
+        <Feedback feedback={feedback} totalFeedback={totalFeedback} />
+      ) : (
+        <Notification>No feedback yet</Notification>
+      )}
+    </div>
+  );
 };
